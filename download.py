584600b160e6acbfe8d96d52f7bebd20113eedb9
laienQmf <105693132+laienQmf@users.noreply.github.com>
2023-02-10 16:36:37 +0800

    Update download.py

--- download.py
+++ download.py
@@ -9,14 +9,14 @@ def download_model():
     os.makedirs("dreambooth_weights/")
     s3 = boto3.resource(service_name='s3',endpoint_url='https://ssssrr.oss-cn-shenzhen.aliyuncs.com',region_name='oss-cn-shenzhen', aws_access_key_id='LTAI5tRwn1RzJSUQgjeYEeqR', aws_secret_access_key='2NyzQeEWu9GmFIVnDlGDnHAZ6sy4WT')
     for bucket in s3.buckets.all():
-        print(bucket.name)
-        print("\n");
+        info(bucket.name)
+        info("\n");
     
     bucket = s3.Bucket("ssssrr")
-    for obj in bucket.objects.filter(Prefix="dreambooth"):
+    for obj in bucket.objects.filter(Prefix="/ssssrr/dreambooth"):
         print(obj.key);
         print('\n');
-        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "dreambooth/"))
+        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "/ssssrr/dreambooth"))
         
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
