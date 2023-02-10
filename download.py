4850d697b992dd6776ee513ea5d0b6673ca5826e
laienQmf <105693132+laienQmf@users.noreply.github.com>
2023-02-10 18:26:34 +0800

    Update download.py

--- download.py
+++ download.py
@@ -10,7 +10,9 @@ def download_model():
     s3 = boto3.resource(service_name='s3',endpoint_url='https://cos.na-siliconvalley.myqcloud.com',region_name='na-siliconvalley', aws_access_key_id='AKIDF9oDQPjm6B0ZsxpZz9UYQzPaT6VhPLu8', aws_secret_access_key='70d6NTKwVlodJn99uPVMxYyDAryviHVW')
     bucket = s3.Bucket("dreambooth-1251401306")
     for obj in bucket.objects.filter(Prefix="dreambooth/"):
+        print(obj.key);
         target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "dreambooth/"))
+        print(target);
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
         if obj.key[-1] == '/':
