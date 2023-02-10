25ba7660546cd9cc7c3871f5ace08f549b495e4a
laienQmf <105693132+laienQmf@users.noreply.github.com>
2023-02-10 17:06:22 +0800

    Update download.py

--- download.py
+++ download.py
@@ -7,16 +7,16 @@ import os
 def download_model():
     # Download the weights from s3 (can be changed to download weights from any cloud)
     os.makedirs("dreambooth_weights/")
-    s3 = boto3.resource(service_name='s3',endpoint_url='https://ssssrr.oss-cn-shenzhen.aliyuncs.com',region_name='oss-cn-shenzhen', aws_access_key_id='LTAI5tRwn1RzJSUQgjeYEeqR', aws_secret_access_key='2NyzQeEWu9GmFIVnDlGDnHAZ6sy4WT')
+    s3 = boto3.resource(service_name='s3',endpoint_url='https://dreambooth-1251401306.cos.na-siliconvalley.myqcloud.com',region_name='na-siliconvalley', aws_access_key_id='AKIDF9oDQPjm6B0ZsxpZz9UYQzPaT6VhPLu8', aws_secret_access_key='70d6NTKwVlodJn99uPVMxYyDAryviHVW')
     for bucket in s3.buckets.all():
         info(bucket.name)
         info("\n");
     
-    bucket = s3.Bucket("ssssrr")
-    for obj in bucket.objects.filter(Prefix="s3://dreambooth"):
+    bucket = s3.Bucket("dreambooth-1251401306")
+    for obj in bucket.objects.filter(Prefix="dreambooth"):
         print(obj.key);
         print('\n');
-        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "ssssrr/dreambooth"))
+        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "dreambooth"))
         
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
