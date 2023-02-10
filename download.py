66e7446251d33c6f740cfa4f0e5aab8af460c363
laienQmf <105693132+laienQmf@users.noreply.github.com>
2023-02-10 18:22:24 +0800

    Update download.py

--- download.py
+++ download.py
@@ -8,19 +8,14 @@ def download_model():
     # Download the weights from s3 (can be changed to download weights from any cloud)
     os.makedirs("dreambooth_weights/")
     s3 = boto3.resource(service_name='s3',endpoint_url='https://cos.na-siliconvalley.myqcloud.com',region_name='na-siliconvalley', aws_access_key_id='AKIDF9oDQPjm6B0ZsxpZz9UYQzPaT6VhPLu8', aws_secret_access_key='70d6NTKwVlodJn99uPVMxYyDAryviHVW')
-    for bucket in s3.buckets.all():
-        info(bucket.name)
-        info("\n");
-        for obj in bucket.objects.filter(Prefix="dreambooth/"):
-            print(obj.key);
-            print('\n');
-            target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "dreambooth/"))
-
-            if not os.path.exists(os.path.dirname(target)):
-                os.makedirs(os.path.dirname(target))
-            if obj.key[-1] == '/':
-                continue
-            bucket.download_file(obj.key, target)
+    bucket = s3.Bucket("dreambooth-1251401306")
+    for obj in bucket.objects.filter(Prefix="dreambooth/"):
+        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "dreambooth/"))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
 # bucket = s3.Bucket("dreambooth-1251401306")
     
 
