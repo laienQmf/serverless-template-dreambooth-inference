d3e6b23ee761d293391409af89593cf03910d0af
laienQmf <105693132+laienQmf@users.noreply.github.com>
2023-02-10 16:16:24 +0800

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ import os
 def download_model():
     # Download the weights from s3 (can be changed to download weights from any cloud)
     os.makedirs("dreambooth_weights/")
-    s3 = boto3.resource(service_name='s3',endpoint_url='https://oss-cn-shenzhen.aliyuncs.com',region_name='oss-cn-shenzhen', aws_access_key_id='LTAI5tRwn1RzJSUQgjeYEeqR', aws_secret_access_key='2NyzQeEWu9GmFIVnDlGDnHAZ6sy4WT')
+    s3 = boto3.resource(service_name='s3',endpoint_url='https://ssssrr.oss-cn-shenzhen.aliyuncs.com',region_name='oss-cn-shenzhen', aws_access_key_id='LTAI5tRwn1RzJSUQgjeYEeqR', aws_secret_access_key='2NyzQeEWu9GmFIVnDlGDnHAZ6sy4WT')
     bucket = s3.Bucket("ssssrr")
     for obj in bucket.objects.filter(Prefix="dreambooth"):
         print(obj.key);
